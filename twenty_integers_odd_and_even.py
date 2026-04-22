d6deb70b69796f812bf8ae55e6109a51516867b3
Acee Zai Mendez <your_email@example.com>
2026-04-22 22:48:18 +0800

    fix variable naming and clean code

--- twenty_integers_odd_and_even.py
+++ twenty_integers_odd_and_even.py
@@ -2,15 +2,15 @@
 
 class NumberSeparator:
     def __init__(self, filename):
-        self.filename = filename
+        self.input_filename = filename
 
-    def separate_even_odd(self):
+    def separate_even_and_odd(self):
         even_file = open("even.txt", "w")
         odd_file = open("odd.txt", "w")
-        file = open(self.filename, "r")
+        input_file = open(self.input_filename, "r")
 
-        for line in file:
-            num = int(line.strip())
+        for number_line in input_file:
+            num = int(number_line.strip())
 
             if num % 2 == 0:
                 even_file.write(str(num) + "\n")
