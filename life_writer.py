5628c84af7b6bd9e0b0d34ba2f5b10a09ba932d9
Acee Zai Mendez <your_email@example.com>
2026-04-23 00:39:05 +0800

    Made while loop for logic

--- life_writer.py
+++ life_writer.py
@@ -4,3 +4,13 @@ class LifeWriter:
 
     def write_lines(self):
         output_file = open(self.filename, "r")
+
+        while True:
+            line_iput = input("Enter line: ")
+            output_file.write(line_input + "\n")
+
+            more_lines = input("Are there ore lines y/n? ")
+
+            if more_lines.lower() == "n":
+                print("File writing complete.")
+l
