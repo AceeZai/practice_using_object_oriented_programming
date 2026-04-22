66e7af43adbcf92c67b71b2f00252589722da4ad
Acee Zai Mendez <your_email@example.com>
2026-04-22 22:25:17 +0800

    Added class

--- twenty_integers_odd_and_even.py
+++ twenty_integers_odd_and_even.py
@@ -1,6 +1,13 @@
 #Separate even and odd numbers from numbers.txt
 
 class NumberSeparator:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def separate_even_odd(self):
+        even_file = open("even.txt", "w")
+        odd_file = open("odd.txt", "w")
+        file = open(self.filename, "r")
 
 
 for line in file:
