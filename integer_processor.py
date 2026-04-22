379f3f27f6c1b5e0111afdda423bc6f287c04363
Acee Zai Mendez <your_email@example.com>
2026-04-23 01:47:37 +0800

    Added new codes

--- integer_processor.py
+++ integer_processor.py
@@ -7,3 +7,14 @@ class IntegerProcessor:
 
         double_file = open("double.txt", "w")
         triple_file = open("triple.txt", "w")
+
+
+        for number_line in input_file:
+            number = int(number_line.strip())
+
+            if number % 2 == 0:
+                squared_value = number ** 2
+                double_file.write(str(squared_value) + "\n")
+            else:
+                cubed_value = number ** 3
+                triple_file.write(str(cubed_value) + "\n")
