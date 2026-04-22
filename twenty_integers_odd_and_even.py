ffc994fd1695c84509ef1d9e0611b3a6c22b16a7
Acee Zai Mendez <your_email@example.com>
2026-04-22 22:09:59 +0800

    Added for loop

--- twenty_integers_odd_and_even.py
+++ twenty_integers_odd_and_even.py
@@ -3,3 +3,11 @@
 #Make a list for even and odd nums
 even_numbers = []
 odd_numbers = []
+
+for num in numbers:
+    num = int(num.strip())
+
+    if num % 2 == 0:
+        even_numbers.append(str(num))
+    else:
+        odd_numbers.append(str(num))
