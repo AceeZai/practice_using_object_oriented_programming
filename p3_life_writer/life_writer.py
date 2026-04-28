aba2da16aaf42b823541f813619a74f1146a77a9
Acee Zai Mendez <your_email@example.com>
2026-04-28 21:35:45 +0800

    Fixed LifeWriter file handling and constructor

--- p3_life_writer/life_writer.py
+++ p3_life_writer/life_writer.py
@@ -1,11 +1,6 @@
 class LifeWriter:
-    def __init__(self):
-        self.filename = r"Traceback (most recent call last):
-  File "C:\Users\Acee Zai Mendez\PycharmProjects\PythonProject\p3_life_writer\life_writer.py", line 16, in <module>
-    life_writer = LifeWriter("p3_life_writer/mylife.txt")
-TypeError: LifeWriter.__init__() takes 1 positional argument but 2 were given
-
-Process finished with exit code 1
+    def __init__(self, filename):
+        self.filename = filename
 
     def write_lines(self):
         with open(self.filename, "w") as output_file:
@@ -16,7 +11,10 @@ Process finished with exit code 1
                 more_lines = input("Are there more lines y/n? ")
                 if more_lines.lower() == "n":
                     break
+
         print("File writing complete at:", self.filename)
 
-life_writer = LifeWriter("p3_life_writer/mylife.txt")
+
+# RUN
+life_writer = LifeWriter("mylife.txt")
 life_writer.write_lines()
